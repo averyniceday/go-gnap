65a5c345c371ffaf87fcd002c2597a771906fe38
Avery Wang <averyjwang@gmail.com>
2023-04-13 08:21:42 -0700

    try this

--- gognap/annotator.go
+++ gognap/annotator.go
@@ -12,17 +12,24 @@ import (
 // Java call: annotator.getAnnotatedRecordsUsingPOST(summaryStatistics, records, "mskcc", true, postIntervalSize, reannotate);
 
 func GetVariantAnnotations(genomicLocations []pubgn.GenomicLocation) []pubgn.VariantAnnotation{
-	token := ""
-	isoformOverrideSource := ""
-	fields := []string{"annotation_summary"}
+	var token = ""
+	fields := make([]string, 0)
+	fields = append(fields,"annotation_summary")
+	var isoformOverrideSource = ""
 	configuration := pubgn.NewConfiguration()
-    	apiClient := pubgn.NewAPIClient(configuration)
-    
-	variantAnnotations, r, err := apiClient.AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationPOST(context.Background()).GenomicLocations(genomicLocations).IsoformOverrideSource(isoformOverrideSource).Token(token).Fields(fields).Execute()
+	apiClient := pubgn.NewAPIClient(configuration)
+    x := apiClient.AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationPOST(context.Background()).GenomicLocations(genomicLocations)
+	x = x.Fields(fields)
+	x = x.IsoformOverrideSource(isoformOverrideSource)
+	x = x.Token(token)
+	variantAnnotations, r, err := x.Execute()
     	if err != nil {
-        	fmt.Fprintf(os.Stderr, "Error when calling `AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationGET``: %v\n", err)
-        	fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-    	}
+			fmt.Fprintf(os.Stderr, "Error when calling `AnnotationControllerApi.FetchVariantAnnotationByGenomicLocationGET``: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+		}
+	fmt.Println("sigh \n\n\n\n")
+	fmt.Println(r.Request)
+	fmt.Println(r.Header)
 	// https://github.com/averyniceday/go-mpath-proto/blob/16d6085cee926ad85fc29c6c62319ee3697edcf2/genome-nexus-public-api/api_annotation_controller.go#L218
     	// response from `FetchVariantAnnotationByGenomicLocationPOST`: []VariantAnnotation, *http.Response, error
 	// VariantAnnotation defined here: https://github.com/averyniceday/go-mpath-proto/blob/16d6085cee926ad85fc29c6c62319ee3697edcf2/genome-nexus-internal-api/model_variant_annotation.go#L21
